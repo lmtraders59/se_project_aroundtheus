350d57e42001f470322bbba15e7b22ea879d2fe3
Louis Maiorino <lmlmlou@gmail.com>
2022-10-17 16:36:11 -0400

    10-17

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,42 +1,48 @@
-console.log("Form validate.js this works superstar :)");
+console.log("Form validate.js this works superstar at Practicum :)");
 
 function setEventlisteners(formEl, options) {
   const { inputSelector } = options;
-  const inputEls = [...formEl.querySelectorAll("inputSelector")];
-      inputEls.forEach = ((inputEl) => {
-          inputEl.addEventListener("e", () => { 
-      });
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("onkeypress", (event) => {
+      function functionCheckIsFieldValid(event, target) {}
+      // when user press on a keyboard, after every key this function is called
+      // functionCheckIsFieldValid(event.target)
+    });
+    console.dir(inputEl);
   });
 }
-console.log(inputEls);
 
 function enableValidation(options) {
-  const formEls = [...document.querySelectorAll("options.formSelector")];
-  formEls.forEach((formEl) => 
-    formEl.addEventListener(submit, (e) => { 
-      e.preventDefault()   
-    })
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach(
+    (formEl) =>
+      formEl.addEventListener("submit", (e) => {
+        e.preventDefault();
+      })
+    //setEventlisteners(formEl, options);
+  );
+}
 
 // look for all inputs inside of the form
-  setEventlisteners (formEl, options) ({
+// setEventlisteners (formEl, options) ({
 // loop thru all inputs to see if they are valid
-  // if all inputs are not valid
-    // get validation message
-    // add error class to input
-    // display error message
-    // disable submit button
-  // if all inputs are valid  
-    // enable submit button
-    // reset error messages
-});
+// if all inputs are not valid
+// get validation message
+// add error class to input
+// display error message
+// disable submit button
+// if all inputs are valid
+// enable submit button
+// reset error messages
 
 const config = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__input",
-  submitButtonSelector: ".popup__button",
-  inactiveButtonClass: "popup__button_disabled",
-  inputErrorClass: "popup__input_type_error",
-  errorClass: "popup__error_visible",
+  formSelector: ".modal__form",
+  inputSelector: ".modal__form-input",
+  submitButtonSelector: ".modal__form-button",
+  inactiveButtonClass: "modal__form-button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
 };
 
 enableValidation(config);

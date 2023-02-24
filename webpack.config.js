37fefd1984c2e223dbaade3b579d3c4cc681de6e
Louis Maiorino <lmlmlou@gmail.com>
2023-02-24 16:23:15 -0500

    2-24 correct webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,67 @@
-class Card {
-  constructor(
-    data,
-    cardSelector,
-    { handleCardClick, handleDeleteClick, handleLike }
-  ) {
-    this.id = data._id;
-    this._link = data.link;
-    this._name = data.name;
-    this._likes = data.likes;
-    // console.log(this._likes.length);
-    this._cardSelector = cardSelector;
-    this._handleCardClick = handleCardClick;
-    this._handleLikeCallback = handleLike;
-    this._handleDeleteClick = handleDeleteClick;
-  }
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-  _handleDelete = () => {
-    this._cardElement.remove();
-  };
-
-  // _handleLikeClick = () => {
-  //   this.cardLikeButton.classList.toggle("card__like-button_on");
-  //   this._handleLike(this);
-  // };
-
-  cardLiked() {
-    return this._likes.some((item) => item._id === this._userId);
-  }
-
-  _renderLikes() {
-    this._likesCount.textContent = this._likes.length;
-    if (this.cardLiked()) {
-      this._cardLikeButton.classList.add("card__like-button_on");
-    } else {
-      this._cardLikeButton.classList.remove("card__like-button_on");
-    }
-  }
-
-  updateLikes(likes) {
-    this._likes = likes;
-    this._renderLikes();
-  }
-
-  _setEventListeners() {
-    // like button
-    this.cardLikeButton = this._cardElement.querySelector(".card__like-button");
-    this.cardLikeButton.addEventListener("click", () =>
-      this._handleLikeCallback(this)
-    );
-
-    // delete card button
-    const cardDeleteButton = this._cardElement.querySelector(
-      ".card__delete-button"
-    );
-    cardDeleteButton.addEventListener("click", this._handleDelete);
-
-    //listen for card image click
-    this._cardImage.addEventListener("click", () => {
-      this._handleCardClick({
-        link: this._cardImage.src,
-        name: this._cardTitle.textContent,
-      });
-    });
-  }
-
-  getView() {
-    this._cardElement = document
-      .querySelector(this._cardSelector)
-      .content.querySelector(".card")
-      .cloneNode(true);
-    this._cardImage = this._cardElement.querySelector(".card__image");
-    this._cardTitle = this._cardElement.querySelector(".card__text");
-    this._setEventListeners();
-    this._cardImage.src = this._link;
-    this._cardImage.alt = this._name;
-    this._cardTitle.textContent = this._name;
-    this._likesCount = this._cardElement.querySelector(".card__like-count");
-    this._likesCount.textContent = this._likes.length;
-    return this._cardElement;
-  }
-}
-
-export default Card;
+module.exports = {
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/pages/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  target: ["web", "es5"],
+  stats: "errors-only",
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  module: {
+    rules: [
+      // this is an array of rules
+      // add an object containing rules for Babel to it
+      {
+        // a regular expression that searches for all js files
+        test: /\.js$/,
+        // all files must be processed by babel-loader
+        loader: "babel-loader",
+        // exclude the node_modules folder, we don't need to process files in it
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+      {
+        // add the rule for processing files
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // path to our index.html file
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(), // connect the plugin for merging CSS files
+  ],
+};
